b793c8e5e104788ced85da3497e883476e70855f
mayur11 <mayurchauhan1107@gmail.com>
2024-10-17 18:30:39 +0530

    update about section

--- assets/js/productlist/productsData.js
+++ assets/js/productlist/productsData.js
@@ -1,9 +1,9 @@
 // productsData.js
-export const products = Array.from({ length: 46 }, (_, i) => ({
+export const products = Array.from({ length: 6 }, (_, i) => ({
   imgSrc: `assets/images/products/${i + 1}.jpg`,
   title: `Product ${i + 1}`, // Placeholder titles
   category: ["Magic", "Attributes"], // Categories can be adjusted accordingly
   rating: (Math.random() * 5).toFixed(2), // Random rating for each product
   price: `${(Math.random() * 100).toFixed(2)}`, // Random price for each product
-  onSale: Math.random() > 0.5, // Randomly decide if it's on sale
+  // onSale: Math.random() > 0.5, // Randomly decide if it's on sale
 }));
